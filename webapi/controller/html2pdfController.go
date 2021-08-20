66170dddaddadfa0abfbeb4a954652185a548e56
Hélio Rugani Brandão <hrugani@gmail.com>
2021-08-20 19:29:06 +0000

    implemented base code for convert endpoint

--- webapi/controller/html2pdfController.go
+++ webapi/controller/html2pdfController.go
@@ -1,11 +1,45 @@
 package controller
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"path"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Convert(c *gin.Context) {
-	c.String(http.StatusOK, "Convert was called...")
+
+	name := c.PostForm("name")
+	email := c.PostForm("email")
+
+	// Multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.String(
+			http.StatusBadRequest,
+			fmt.Sprintf("request err: %s", err.Error()+"\n"),
+		)
+		return
+	}
+	files := form.File["files"]
+
+	for _, file := range files {
+		filename := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, filename); err != nil {
+			c.String(
+				http.StatusBadRequest,
+				fmt.Sprintf("upload file err: %s", err.Error()+"\n"),
+			)
+			return
+		}
+	}
+
+	c.String(
+		http.StatusOK,
+		fmt.Sprintf("Uploaded successfully %d files with fields name=%s and email=%s\n", len(files),
+			name,
+			email))
 }
