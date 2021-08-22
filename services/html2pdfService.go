a0086d4905adcba11e8598225cd1230a205689db
Hélio Rugani Brandão <hrugani@gmail.com>
2021-08-22 03:42:32 +0000

    added logging of service calling.

--- services/html2pdfService.go
+++ services/html2pdfService.go
@@ -1,5 +1,8 @@
 package services
 
+import "log"
+
 func HtmlText2Pdf(workDirName, fName string) ([]byte, error) {
+	log.Default().Printf("service Zip2Pdf was called. WorkDir= %s, File= %s", workDirName, fName)
 	return nil, nil
 }
--- services/zip2pdfService.go
+++ services/zip2pdfService.go
@@ -1,6 +1,9 @@
 package services
 
+import "log"
+
 
 func Zip2Pdf(workdirName, filename string) ([]byte, error) {
+	log.Default().Printf("service Zip2Pdf was called. WorkDir= %s, File= %s", workdirName, filename )
 	return nil, nil
 }
