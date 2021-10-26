667e94d463ce590c6c43bcacaf7a930e1f011dcd
Hélio Rugani Brandão <hrugani@gmail.com>
2021-10-26 02:45:23 +0000

    recreated zip files for testing and adjust scripts of convert/merge simulations

--- scripts/curl-simulate-html2pdf-request.sh
+++ scripts/curl-simulate-html2pdf-request.sh
@@ -1,4 +1,4 @@
 # simulates a post http request to /merge endpoint and uploading a mergepdfs-example-1.zip file as the input data
 cd ../zip-input-files-examples 
-curl -X POST http://localhost:8080/html2pdf?preffix=preffix1234 -F files=@html2pdf-example-1.zip -H "Content-Type: multipart/form-data" -o comverted.pdf
+curl -X POST http://localhost:8080/html2pdf?preffix=preffix1234 -F files=@html2pdf-example-2.zip -H "Content-Type: multipart/form-data" -o comverted.pdf
 cd ../scripts
--- scripts/curl-simulate-mergepdfs-request.sh
+++ scripts/curl-simulate-mergepdfs-request.sh
@@ -1,4 +1,4 @@
 # simulates a post http request to /merge endpoint and uploading a mergepdfs-example-1.zip file as the input data
 cd ../zip-input-files-examples 
-curl -X POST http://localhost:8080/merge?preffix=preffix1234 -F files=@mergepdf-example-1.zip -H "Content-Type: multipart/form-data" -o merged.pdf
+curl -X POST http://localhost:8080/merge?preffix=preffix1234 -F files=@mergepdf-example-2.zip -H "Content-Type: multipart/form-data" -o merged.pdf
 cd ../scripts
